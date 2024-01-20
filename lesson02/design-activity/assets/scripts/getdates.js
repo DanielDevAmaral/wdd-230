d9c50539c364f7a400d88e57ec3882da6b09c086
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2024-01-19 22:00:07 -0300

    lesson 2 complete

---
+++ lesson02/design-activity/assets/scripts/getdates.js
@@ -0,0 +1,5 @@
+
+footerDetails = `&copy${new Date().getFullYear()} | Daniel Amaral | Brazil | Last modified: ${document.lastModified}`
+document.querySelector("#currentYear").innerHTML += footerDetails;
+
+
