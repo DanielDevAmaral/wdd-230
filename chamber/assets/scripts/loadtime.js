767888619b93ea77489c04e0888ba0109846333f
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2024-03-07 23:27:36 -0300

    join page

---
+++ chamber/assets/scripts/loadtime.js
@@ -0,0 +1,5 @@
+  // Get the hidden input element
+  const loadTimeField = document.getElementById("hidden");
+
+  // Set the value to the current timestamp in milliseconds
+  loadTimeField.value = Date.now();
