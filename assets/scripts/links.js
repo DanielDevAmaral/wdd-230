48e9b57102498c15fd430430bfdb8a9705be613b
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2024-03-09 23:19:54 -0300

    update json

--- assets/scripts/links.js
+++ assets/scripts/links.js
@@ -1,8 +1,12 @@
 const url = "https://danieldevamaral.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const linksURL = "https://danieldevamaral.github.io/wdd230/data/links.json";
 
 async function getData() {
-    const result = await fetch(url);
+    const result = await fetch(linksURL);
     const data = await result.json();
     console.table(data)
-  }
+    displayLinks(data)
+
+}
+
+getData()
