ab4ce2367872de26087ab47574ad58a2cd187f2d
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2024-03-11 20:55:37 -0300

    data members created

---
+++ chamber/assets/scripts/members.js
@@ -0,0 +1,37 @@
+const url = "https://danieldevamaral.github.io/wdd230/";
+const linksURL = "https://danieldevamaral.github.io/wdd230/data/members.json";
+
+const ul = document.querySelector(".activities");
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        const li = document.createElement("li");
+        const aLinkone = document.createElement("a");
+        const aLinktwo = document.createElement("a");
+
+        (week.links).forEach((element, index) => {
+            if (index === 0) {
+                aLinkone.setAttribute("href", `${element.url}`);
+                aLinkone.textContent = `${element.title}`;
+            } else if (index === 1) {
+                aLinktwo.setAttribute("href", `${element.url}`);
+                aLinktwo.textContent = ` | ${element.title}`;
+            }
+        });
+
+        li.textContent = `${week.lesson}: `;
+        li.appendChild(aLinkone);
+        li.appendChild(aLinktwo);
+        ul.appendChild(li);
+    });
+};
+
+
+async function getData() {
+    const result = await fetch(linksURL);
+    const data = await result.json();
+    displayLinks(data.lessons)
+
+}
+
+getData()
