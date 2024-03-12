a5c0c543faf3b8d9da745bf030c4f2e2179a751f
DanielDevAmaral <Daniel.amaral1928@outlook.com>
2024-03-11 22:08:30 -0300

    update image members

--- chamber/assets/scripts/members.js
+++ chamber/assets/scripts/members.js
@@ -1,12 +1,23 @@
-const url = "https://danieldevamaral.github.io/wdd230/";
-const linksURL = "https://danieldevamaral.github.io/wdd230/data/members.json";
+const url = "https://danieldevamaral.github.io/wdd230/data/members.json";
 
-const main = document.querySelector("main");
+async function getData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.empresas);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Handle the error here, like displaying an error message to the user
+  }
+}
 
-const displayCompanies = (companies) => {
+function displayCompanies(companies) {
   companies.forEach((company) => {
+    const companyDiv = document.createElement("div");
+    companyDiv.setAttribute("class", "company"); // Add a class for styling
+
     const image = document.createElement("img");
-    image.setAttribute("src", `${company.image}`); // Corrected attribute
+    image.setAttribute("src", `${company.image}`);
 
     const name = document.createElement("h3");
     name.textContent = `${company.name}`;
@@ -14,20 +25,17 @@ const displayCompanies = (companies) => {
     const address = document.createElement("p");
     address.textContent = `${company.address}`;
 
-    const phoneNumber = document.createElement("p"); // Corrected spelling
+    const phoneNumber = document.createElement("p");
     phoneNumber.textContent = `${company.phone_number}`;
 
     const website = document.createElement("p");
     website.textContent = `${company.website}`;
 
-    const membershipLevel = document.createElement("h4"); // Corrected CamelCase
+    const membershipLevel = document.createElement("h4");
     membershipLevel.textContent = `${company.membership_level}`;
 
     const additionalInformation = document.createElement("p");
     additionalInformation.textContent = `${company.additional_information}`;
-
-    const companyDiv = document.createElement("div");
-    companyDiv.setAttribute("class", "companies");
 
     companyDiv.appendChild(image);
     companyDiv.appendChild(name);
@@ -37,19 +45,8 @@ const displayCompanies = (companies) => {
     companyDiv.appendChild(membershipLevel);
     companyDiv.appendChild(additionalInformation);
 
-    main.appendChild(companyDiv);
+    document.querySelector("main").appendChild(companyDiv);
   });
-};
-
-async function getData() {
-  try {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayCompanies(data.empresas);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    // Handle the error here, like displaying an error message to the user
-  }
 }
 
 getData();
